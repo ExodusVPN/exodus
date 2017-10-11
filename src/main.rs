d978a109ca6696925107a1c4a66ce2b35d2f585d
luozijun <gnulinux@126.com>
2017-10-11 16:35:12 +0800

    update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ fn main(){
 #[cfg(all(target_family = "unix", any(target_os = "macos", target_os = "ios"
                                       target_os = "freebsd", target_os = "bitrig", 
                                       target_os = "dragonfly", 
-                                      target_os = "netbsd", target_os = "openbsd"
-    )))]
+                                      target_os = "netbsd", target_os = "openbsd")
+))]
 fn main {
     
 }
@@ -26,12 +26,12 @@ fn main {
 
 #[cfg(all(target_family = "unix", any(target_os = "linux", target_os = "android")))]
 fn main {
-    
+    unimplemented!();
 }
 
-#[cfg(any(  not(target_family = "windows", target_family = "unix")
-            not(target_os = "macos", target_os = "ios", target_os = "linux", target_os = "android")
-             ))]
+#[cfg(any(not(target_family = "windows", target_family = "unix")
+          not(target_os = "macos", target_os = "ios", target_os = "linux", target_os = "android")
+))]
 fn main (){
     unimplemented!();
 }
