d777aa6d2e2b9b3af962aa7be37afa71b54bccd5
luozijun <gnulinux@126.com>
2017-10-17 22:16:55 +0800

    Update src/net/packet/mod.rs

--- src/net/packet/mod.rs
+++ src/net/packet/mod.rs
@@ -6,3 +6,20 @@ pub use self::ethernet::EthernetPacket as Packet;
 
 
 
+
+// Layer 1:
+//     ethernet::EthernetPacket
+// Layer 2:
+//         ethernet::EthernetFrame
+//         arp::ArpPacket
+//         ndp::NdpPacket
+// Layer 3:
+//             ip::IpPacket
+// Layer 4:
+//                 ip::v4::tcp::IcmpPacket
+//                 ip::v4::tcp::UdpPacket
+//                 ip::v4::tcp::TcpPacket
+// Layer 5:
+//                 ip::Protocol::HTTP
+
+
