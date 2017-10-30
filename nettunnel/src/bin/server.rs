cdf40a96b6a193d3496ccfa520dcd49b409b53e6
luozijun <gnulinux@126.com>
2017-10-30 08:16:15 +0800

    Update nettunnel/src/bin/server.rs & nettunnel/src/bin/client.rs

--- nettunnel/src/bin/server.rs
+++ nettunnel/src/bin/server.rs
@@ -2,7 +2,6 @@
 
 extern crate taptun;
 extern crate netpacket;
-extern crate crypto;
 
 extern crate futures;
 
@@ -15,196 +14,174 @@ extern crate byteorder;
 #[macro_use(trace, debug, info, warn, error, log)]
 extern crate logging;
 
+extern crate libc;
+extern crate mio;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate bincode;
 
 
 use std::fmt;
 use std::convert::AsMut;
 use std::io::{Read, Write};
-use std::net::Ipv4Addr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use std::rc::Rc;
+use std::sync::{ Arc, Mutex };
+use std::cell::{ RefCell, RefMut, Cell };
+use std::borrow::{ BorrowMut, Borrow };
+use std::thread;
+use std::collections::HashMap;
+use std::mem::transmute;
+use std::sync::mpsc::channel;
+
+
 use tokio_core::net::{TcpStream, TcpListener};
 use tokio_io::io::{read_exact, read_to_end, read, write_all, Window, copy};
+
+use ::futures::sync::oneshot;
+use tokio_core::net::UdpSocket;
 
 use futures::stream;
 use futures::future;
 use futures::{Future, Stream, Poll, Async};
 use byteorder::{BigEndian, ReadBytesExt, ByteOrder};
 
+use mio::Evented;
+
 use taptun::tun;
 
 
 /**
-    sudo route add 10.0.0.1/24 -interface utun10
-    curl "10.0.0.50:80"
-    ping 10.0.0.50
-    ping 10.0.0.1
+    sudo route add 10.200.200.1/32 -interface utun10
+    curl "10.200.200.1:80"
+    ping 10.200.200.1
     
     sudo route get default
     sudo route delete default
     sudo route add default -interface utun10
     netstat -nr
+
+GNU/Linux:
+    ip -4 route list 0/0 | awk '{print $3}'
+macOS:
+    route -n get default | grep gateway | awk '{print $2}'
+
+
+sudo iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o utun10 -j MASQUERADE
+
+sudo route add default -net 192.168.0.1
+
 **/
 
-pub struct Packet([u8; 1500]);
-
-impl AsMut<[u8]> for Packet{
-    #[inline]
-    fn as_mut(&mut self) -> &mut [u8] {
-        &mut self.0
-    }
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Cmd {
+    AllocIp,
+    ExchangeData
 }
-
-impl fmt::Display for Packet {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "[{}]", self.0.iter().map(|n| n.to_string() ).collect::<Vec<String>>().join(", "))
-    }
-}
-
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Method {
-    GetPeers,
-    Transfer
-}
-
-
-
-
-use std::sync::{Arc, Mutex};
-use std::cell::Cell;
-use std::cell::RefCell;
-use std::rc::Rc;
-use std::borrow::BorrowMut;
-use std::borrow::Borrow;
-use std::cell::RefMut;
-use std::thread;
-
-use ::futures::sync::oneshot;
-use std::collections::HashMap;
-
-pub struct Vpn<'a, 'b> {
-    registry : Arc<Mutex<HashMap<u32, TcpStream>>>,
-    best_ip  : Arc<Mutex<u32>>,
-    iface_hub_sender  : futures::sync::oneshot::Sender<&'a [u8]>,
-    iface_hub_receiver: futures::sync::oneshot::Receiver<&'b [u8]>
-}
-
-impl<'a, 'b> Vpn<'a, 'b> {
-    pub fn new() -> Result<Vpn<'a, 'b>, ::std::io::Error> {
-        
-        let registry: Arc<Mutex<HashMap<u32, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
-        let best_ip = Arc::new(Mutex::new(u32::from(Ipv4Addr::new(10, 0, 0, 10))));
-
-        let (sender, receiver) = oneshot::channel::<&[u8]>();
-
-        Ok(Vpn {
-            registry: registry,
-            best_ip : best_ip,
-            iface_hub_sender  : unsafe { ::std::mem::transmute(sender) },
-            iface_hub_receiver: unsafe { ::std::mem::transmute(receiver) }
-        })
-    }
-
-    pub fn ip_gen(&self) -> Option<Ipv4Addr> {
-        let mut best_ip_clone = self.best_ip.lock().unwrap();
-        *best_ip_clone += 1;
-        Some(Ipv4Addr::from(*best_ip_clone))
-    }
-
-    pub fn add_peer(&mut self, peer: TcpStream, ipv4_addr: ::std::net::Ipv4Addr) -> bool {
-        let peer_id = u32::from(ipv4_addr);
-        let mut registry = self.registry.lock().unwrap();
-
-        registry.insert(peer_id, peer).is_none()
-    }
-}
-
-fn on_connection(handle: tokio_core::reactor::Handle,
-                stream : TcpStream, 
-                addr   : ::std::net::SocketAddr,
-                device_clone: Arc<Mutex<tun::Device>>) 
-                            -> Box<future::Future<Item=(), Error=()>> {
-        Box::new(read_exact(stream, [0u8; 4])
-            .and_then(|(stream, buf)|{
-                let size = BigEndian::read_u16(&buf);
-
-                info!("size: {:?}  ", size);
-                read(stream, vec![0u8; size as usize]).and_then(move |(stream, data, size)|{
-                    info!("buf: {:?}", &buf[..2]);
-                    info!("packet: {:?}  ", &data[..size]);
-                    let mut device = device_clone.lock().unwrap();
-                    device.write(&data[..size]);
-                    future::ok(())
-                })
-            })
-            .or_else(|_| {
-                future::err(())
-            }))
-    }
 
 
 fn main () {
-    logging::init(Some("info")).unwrap();
+    logging::init(Some("debug")).unwrap();
 
-    let mut lp = tokio_core::reactor::Core::new().unwrap();
-    let handle = lp.handle();
-
-    let addr = "127.0.0.1:9000".parse::<::std::net::SocketAddr>().unwrap();
-    let listener: TcpListener = TcpListener::bind(&addr, &handle).unwrap();
-    info!("Server will running on {:?} ...", addr);
-
-    let mut vpn = Vpn::new().unwrap();
-    let fut1 = listener.incoming()
-        .and_then(move |(stream, addr)| {
-            let ip = vpn.ip_gen().unwrap();
-            vpn.add_peer(stream, ip);
-            let registry = vpn.registry.lock().unwrap();
-            warn!("Registry Keys: {:?}", registry.keys());
-            future::ok(())
-        })
-        .or_else(|_| future::err(()))
-        .for_each(|a| Ok(()));
-
-    lp.handle().spawn(fut1);
-
+    if unsafe { libc::getuid() } > 0 {
+        error!("请以管理员身份运行该程序！");
+        ::std::process::exit(1);
+    }
 
     let mut config = tun::Configuration::default();
-        config.address(Ipv4Addr::new(10, 0, 0, 1))
-               .netmask(Ipv4Addr::new(255, 255, 255, 0))
-               .destination(Ipv4Addr::new(0, 0, 0, 0))
-               .mtu(1500)
-               .name("utun10")
-               .up();
-        
+    config.address(Ipv4Addr::new(10, 200, 200, 1))
+           .netmask(Ipv4Addr::new(255, 255, 255, 0))
+           .destination(Ipv4Addr::new(0, 0, 0, 0))
+           .mtu(1500)
+           .name("utun10")
+           .up();
     let dev_res = tun::create(&config);
     if dev_res.is_err() {
         error!("虚拟网络设备创建失败: {:?}", dev_res);
-        // return Err(::std::io::Error::new(::std::io::ErrorKind::Other, "Oh, no ...."));
-        return ();
+        ::std::process::exit(1);
     }
+    let mut tun_device = dev_res.unwrap();
+    info!("虚拟网络设备 utun10 初始化完成，网关: 10.200.200.1 ...");
 
-    // let mut tokio_device = tun::tokio::Device::new(dev_res.unwrap(), &handle.clone()).unwrap();
-    let mut device = dev_res.unwrap();
-    let mut buf = [0u8; 1500];
+    let addr = "0.0.0.0:9250".parse::<::std::net::SocketAddr>().unwrap();
+    let udp_socket_raw_fd = mio::net::UdpSocket::bind(&addr).unwrap();
+    info!("UDP Socket Listening on: {:?} ...", addr);
 
-    thread::spawn(move || {
-        loop {
-            match device.read(&mut buf) {
-                Ok(size) => {
-                    info!("Read: {:?} ", &buf[..size]);
-                },
-                Err(e) => {
-                    error!("{:?}", e);
-                }
-            };
-        }
-    });
+    let poll = mio::Poll::new().unwrap();
 
-    thread::spawn(move ||{
+    const TUN_TOKEN: mio::Token = mio::Token(0);
+    const UDP_TOKEN: mio::Token = mio::Token(1);
 
-    });
+    tun_device.register(&poll, TUN_TOKEN, mio::Ready::readable(), mio::PollOpt::level()).unwrap();
+    poll.register(&udp_socket_raw_fd, UDP_TOKEN, mio::Ready::readable(), mio::PollOpt::level()).unwrap();
 
+
+    let mut events = mio::Events::with_capacity(1024);
+    let mut registry: HashMap<Ipv4Addr, SocketAddr> = HashMap::new();
+    let mut buf = [0u8; 1600];
+
+    info!("Ready for transmission.");
+
+    let mut best_ip_number = u32::from(Ipv4Addr::new(10, 200, 200, 9));
 
     loop {
-        lp.turn(None);
+        poll.poll(&mut events, None).unwrap();
+        for event in events.iter() {
+            match event.token() {
+                UDP_TOKEN => {
+                    let (size, remote_socket_addr) = udp_socket_raw_fd.recv_from(&mut buf).unwrap();
+                    if size == 0 || remote_socket_addr.ip().is_ipv6() {
+                        debug!("Error Pakcet: {:?}", &buf[..size]);
+                        continue;
+                    }
+                    let cmd = buf[0];
+                    match cmd {
+                        1 => {
+                            best_ip_number += 1;
+                            if let IpAddr::V4(remote_ip) = remote_socket_addr.ip() {
+                                let remote_ip_number = u32::from(remote_ip);
+                                let msg = [1, 
+                                    ((best_ip_number >> 24) & 0xff) as u8, ((best_ip_number >> 16) & 0xff) as u8,
+                                    ((best_ip_number >> 8) & 0xff) as u8, (best_ip_number & 0xff) as u8,
+                                    ((remote_ip_number >> 24) & 0xff) as u8, ((remote_ip_number >> 16) & 0xff) as u8,
+                                    ((remote_ip_number >> 8) & 0xff) as u8, (remote_ip_number & 0xff) as u8,
+                                ];
+                                udp_socket_raw_fd.send_to(&msg, &remote_socket_addr);
+                                let internal_ip = Ipv4Addr::from(best_ip_number);
+                                debug!("为 {:?} 分配虚拟地址 {:?}", remote_socket_addr, internal_ip);
+                                registry.insert(internal_ip, remote_socket_addr);
+                            }
+                        },
+                        2 => {
+                            let data = &buf[1..size];
+                            match tun_device.write(data) {
+                                Ok(_) => {},
+                                Err(e) => {
+                                    debug!("虚拟网络设备写入数据失败: {:?}", e);
+                                }
+                            };
+                        },
+                        _ => continue
+                    };
+                },
+                TUN_TOKEN => {
+                    let size: usize = tun_device.read(&mut buf).unwrap();
+                    if size == 0 {
+                        continue;
+                    }
+
+                    for (internal_ip, remote_socket_addr) in &registry {
+                        let mut data: Vec<u8> = vec![2];
+                        data.extend(&buf[..size]);
+                        udp_socket_raw_fd.send_to(&data, remote_socket_addr).unwrap();
+                        debug!("转发数据至 {:?} ({:?}) ...", internal_ip, remote_socket_addr);
+                    }
+                },
+                _ => unreachable!(),
+            }
+        }
     }
+  
 }
