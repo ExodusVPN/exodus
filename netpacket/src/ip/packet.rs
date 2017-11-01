aceee52ff73e02600f00211b72edea5cc36b64e1
luozijun <gnulinux@126.com>
2017-11-01 18:04:58 +0800

    Update netpacket/src/ip/packet.rs

--- netpacket/src/ip/packet.rs
+++ netpacket/src/ip/packet.rs
@@ -1,5 +1,5 @@
 
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, ByteOrder};
 
 use std::mem::transmute;
 
@@ -46,6 +46,13 @@ pub struct Ipv4Packet<'a, 'b> {
     /// Differentiated Services Code Point (DSCP) & Explicit Congestion Notification (ECN),
     /// Originally defined as the Type of service (ToS) field.
     dscp_ecn: u8,               //  6 bits, 2 bits
+    /// This 16-bit field defines the entire packet size in bytes, including header and data. 
+    /// The minimum size is 20 bytes (header without data) and the maximum is 65,535 bytes. 
+    /// All hosts are required to be able to reassemble datagrams of size up to 576 bytes, 
+    /// but most modern hosts handle much larger packets. 
+    /// Sometimes links impose further restrictions on the packet size, 
+    /// in which case datagrams must be fragmented. 
+    /// Fragmentation in IPv4 is handled in either the host or in routers.
     total_length: u16,          // 16 bits
     identification: u16,        // 16 bits
     flags_fragment_offset: u16, //  3 bits, 13 bits
@@ -63,12 +70,14 @@ pub struct Ipv4Packet<'a, 'b> {
 /// https://en.wikipedia.org/wiki/IPv6_packet
 #[derive(Debug, PartialEq, Eq)]
 pub struct Ipv6Packet<'a> {
-    version: u8,         //  4 bits
-    traffic_class: u8,   //  8 bits
-    flow_label: u32,     // 20 bits
-    payload_length: u16, // 16 bits
-    next_header: u8,     //  8 bits
-    hoplimit: u8,        //  8 bits
+    version: u8,         //   4 bits
+    traffic_class: u8,   //   8 bits
+    flow_label: u32,     //  20 bits
+    /// The size of the payload in octets, including any extension headers. 
+    /// The length is set to zero when a Hop-by-Hop extension header carries a Jumbo Payload option.
+    payload_length: u16, //  16 bits
+    next_header: u8,     //   8 bits
+    hoplimit: u8,        //   8 bits
     src_ip  : u128,      // 128 bits
     dst_ip  : u128,      // 128 bits
     payload : &'a [u8]
@@ -88,34 +97,26 @@ impl <'a, 'b>Ipv4Packet<'a, 'b> {
         if payload.len() < Ipv4Packet::min_size() {
             return Err(::std::io::Error::new(::std::io::ErrorKind::Other, "size error ..."));
         }
+
         let version_ihl = payload[0];
+        let ihl = version_ihl & 0b_0000_1111;
 
         if (version_ihl >> 4) != 4 {
             return Err(::std::io::Error::new(::std::io::ErrorKind::Other, "size error ..."));
         }
-        let ihl = u8::from_str_radix(&format!("{:08b}", version_ihl)[4..8], 2).unwrap();
 
         let dscp_ecn    = payload[1];
-
-        let mut total_length_bytes = &payload[2..4];
-        let total_length: u16          = total_length_bytes.read_u16::<BigEndian>().unwrap();
-        let mut identification_bytes = &payload[4..6];
-        let identification: u16        = identification_bytes.read_u16::<BigEndian>().unwrap();
-
-        let mut flags_fragment_offset_bytes = &payload[6..8];
-        let flags_fragment_offset: u16 = flags_fragment_offset_bytes.read_u16::<BigEndian>().unwrap();
+        let total_length: u16   = BigEndian::read_u16(&payload[2..4]);
+        let identification: u16 = BigEndian::read_u16(&payload[4..6]);
+        let flags_fragment_offset: u16 = BigEndian::read_u16(&payload[6..8]);
 
         let time_to_live = payload[8];
         let protocol     = payload[9];
+        let header_checksum: u16 = BigEndian::read_u16(&payload[10..12]);
 
-        let mut header_checksum = &payload[10..12];
-        let header_checksum: u16 = header_checksum.read_u16::<BigEndian>().unwrap();
+        let src_ip: u32 = BigEndian::read_u32(&payload[12..16]);
+        let dst_ip: u32 = BigEndian::read_u32(&payload[16..20]);
 
-        let mut src_ip_bytes = &[payload[12], payload[13], payload[14], payload[15]][..];
-        let src_ip: u32 = src_ip_bytes.read_u32::<BigEndian>().unwrap();
-        let mut dst_ip_bytes = &[payload[16], payload[17], payload[18], payload[19]][..];
-        let dst_ip: u32 = dst_ip_bytes.read_u32::<BigEndian>().unwrap();
-        
         let options: Option<Options<'a>>;
         let header_length: usize;
         if ihl >= 5 {
@@ -183,13 +184,10 @@ impl <'a, 'b>Ipv4Packet<'a, 'b> {
         self.total_length
     }
     pub fn flags(&self) -> u8 {
-        // u8::from_str_radix(&format!("{:016b}", self.flags_fragment_offset)[0..3], 2).unwrap()
         (self.flags_fragment_offset >> 13) as u8
     }
     pub fn fragment_offset(&self) -> u16 {
-        u16::from_str_radix(&format!("{:016b}", self.flags_fragment_offset)[3..16], 2).unwrap()
-        // ((self.flags_fragment_offset << 8) >> 5) + ((self.flags_fragment_offset & 0xff) << 0)
-        // (self.flags_fragment_offset & 0b_0001_1111) + (self.flags_fragment_offset & 0xff)
+        self.flags_fragment_offset & 0b_0001_1111
     }
     pub fn time_to_live(&self) -> u8 {
         self.time_to_live
@@ -220,21 +218,83 @@ impl <'a, 'b>Ipv4Packet<'a, 'b> {
     }
 }
 
-
 impl <'a>Ipv6Packet<'a> {
     #[allow(unused_variables)]
     pub fn from_bytes(payload: &[u8]) -> Result<Self, ::std::io::Error> {
-        println!("[WARN] 检测到不支持的 IPv6 Packet: {:?}", payload);
+        if payload.len() < 40 {
+            return Err(::std::io::Error::new(::std::io::ErrorKind::Other, "size error ..."));
+        }
+        
+        let version       = payload[0] >> 4;
+        let traffic_class = (payload[0] & 0b_0000_1111) | (payload[1] >> 4);
+        let flow_label    = (((payload[1] & 0b_0000_1111) as u32) << 16) | ((payload[2] as u32) << 8) | (payload[3] as u32);
+        let payload_length: u16 = BigEndian::read_u16(&payload[4..6]);
+
+        if payload.len() != payload_length as usize {
+            return Err(::std::io::Error::new(::std::io::ErrorKind::Other, "size error ..."));
+        }
+        
+        let next_header = payload[6];
+        let hoplimit    = payload[7];
+        
+        let src_ip6 = ::std::net::Ipv6Addr::new(
+            BigEndian::read_u16(&payload[ 8..10]), BigEndian::read_u16(&payload[10..12]), BigEndian::read_u16(&payload[12..14]),
+            BigEndian::read_u16(&payload[14..16]), BigEndian::read_u16(&payload[16..18]), BigEndian::read_u16(&payload[18..20]),
+            BigEndian::read_u16(&payload[20..22]), BigEndian::read_u16(&payload[22..24])
+        );
+        let src_ip: u128 = u128::from(src_ip6);
+
+        let dst_ip6 = ::std::net::Ipv6Addr::new(
+            BigEndian::read_u16(&payload[24..26]), BigEndian::read_u16(&payload[26..28]), BigEndian::read_u16(&payload[28..30]),
+            BigEndian::read_u16(&payload[30..32]), BigEndian::read_u16(&payload[32..34]), BigEndian::read_u16(&payload[34..36]),
+            BigEndian::read_u16(&payload[36..38]), BigEndian::read_u16(&payload[38..40])
+        );
+        let dst_ip: u128 = u128::from(dst_ip6);
+
+        Ok(Ipv6Packet {
+            version: version,
+            traffic_class: traffic_class,
+            flow_label: flow_label,
+            payload_length: payload_length,
+            next_header: next_header,
+            hoplimit: hoplimit,
+            src_ip: src_ip,
+            dst_ip: dst_ip,
+            payload: unsafe { transmute(&payload[40..]) }
+        })
+    }
+    pub fn as_bytes(&self) -> &[u8] {
         unimplemented!();
     }
 
-    pub fn as_bytes(&self) -> &[u8] {
-        unimplemented!();
+    pub fn version(&self) -> u8 {
+        self.version
+    }
+    pub fn traffic_class(&self) -> u8 {
+        self.traffic_class
+    }
+    pub fn flow_label(&self) -> u32 {
+        self.flow_label
+    }
+    pub fn payload_length(&self) -> u16 {
+        self.payload_length
+    }
+    pub fn next_header(&self) -> u8 {
+        self.next_header
+    }
+    pub fn hoplimit(&self) -> u8 {
+        self.hoplimit
+    }
+    pub fn src_ip(&self) -> u128 {
+        self.src_ip
+    }
+    pub fn dst_ip(&self) -> u128 {
+        self.dst_ip
     }
     pub fn payload(&self) -> &'a [u8] {
         &self.payload
     }
-
+    
     pub fn verify_checksum(&self) -> bool {
         unimplemented!();
     }
