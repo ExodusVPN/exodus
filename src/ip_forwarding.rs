3aebe95988753a09ff5c01628d556c080a2551b0
luozijun <luozijun.assistant@gmail.com>
2019-06-24 21:02:21 +0800

    Update sysctl

--- src/ip_forwarding.rs
+++ src/ip_forwarding.rs
@@ -1,78 +1,192 @@
-use sysctl;
-
-use std::io;
-
 
 #[cfg(any(target_os = "ios", target_os = "macos"))]
-const IPV4_KEY: &str = "net.inet.ip.forwarding";
-#[cfg(any(target_os = "ios", target_os = "macos"))]
-const IPV6_KEY: &str = "net.inet6.ip.forwarding";
+mod sys {
+    use sysctl;
+    
+    use std::io;
 
-#[cfg(any(target_os = "android", target_os = "linux"))]
-const IPV4_KEY: &str = "net.ipv4.ip_forward";
-#[cfg(any(target_os = "android", target_os = "linux"))]
-const IPV6_KEY: &str = "net.ipv6.ip_forward";
+    #[cfg(any(target_os = "ios", target_os = "macos"))]
+    const IPV4_KEY: &str = "net.inet.ip.forwarding";
+    #[cfg(any(target_os = "ios", target_os = "macos"))]
+    const IPV6_KEY: &str = "net.inet6.ip.forwarding";
+
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    const IPV4_KEY: &str = "net.ipv4.ip_forward";
+    #[cfg(any(target_os = "android", target_os = "linux"))]
+    const IPV6_KEY: &str = "net.ipv6.ip_forward";
+
+    #[inline]
+    fn value_to_bool(value: sysctl::CtlValue) -> Result<bool, io::Error> {
+        match value {
+            sysctl::CtlValue::Int(n) => Ok(n == 1),
+            sysctl::CtlValue::Uint(n) => Ok(n == 1),
+            sysctl::CtlValue::Long(n) => Ok(n == 1),
+            sysctl::CtlValue::Ulong(n) => Ok(n == 1),
+            
+            sysctl::CtlValue::U8(n) => Ok(n == 1),
+            sysctl::CtlValue::U16(n) => Ok(n == 1),
+            sysctl::CtlValue::U32(n) => Ok(n == 1),
+            sysctl::CtlValue::U64(n) => Ok(n == 1),
+
+            sysctl::CtlValue::S8(n) => Ok(n == 1),
+            sysctl::CtlValue::S16(n) => Ok(n == 1),
+            sysctl::CtlValue::S32(n) => Ok(n == 1),
+            sysctl::CtlValue::S64(n) => Ok(n == 1),
+            _ => Err(io::Error::from(io::ErrorKind::InvalidData)),
+        }
+    }
+
+    #[inline]
+    pub fn ipv4_forwarding() -> Result<bool, io::Error> {
+        sysctl::value(IPV4_KEY)
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
+    }
 
 
-#[inline]
-fn value_to_bool(value: sysctl::CtlValue) -> Result<bool, io::Error> {
-    match value {
-        sysctl::CtlValue::Int(n) => Ok(n == 1),
-        sysctl::CtlValue::Uint(n) => Ok(n == 1),
-        sysctl::CtlValue::Long(n) => Ok(n == 1),
-        sysctl::CtlValue::Ulong(n) => Ok(n == 1),
-        
-        sysctl::CtlValue::U8(n) => Ok(n == 1),
-        sysctl::CtlValue::U16(n) => Ok(n == 1),
-        sysctl::CtlValue::U32(n) => Ok(n == 1),
-        sysctl::CtlValue::U64(n) => Ok(n == 1),
+    #[inline]
+    pub fn enable_ipv4_forwarding() -> Result<bool, io::Error> {
+        sysctl::set_value(IPV4_KEY, sysctl::CtlValue::Int(1))
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
+    }
 
-        sysctl::CtlValue::S8(n) => Ok(n == 1),
-        sysctl::CtlValue::S16(n) => Ok(n == 1),
-        sysctl::CtlValue::S32(n) => Ok(n == 1),
-        sysctl::CtlValue::S64(n) => Ok(n == 1),
-        _ => Err(io::Error::from(io::ErrorKind::InvalidData)),
+    #[inline]
+    pub fn disable_ipv4_forwarding() -> Result<bool, io::Error> {
+        sysctl::set_value(IPV4_KEY, sysctl::CtlValue::Int(0))
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
+    }
+
+    #[inline]
+    pub fn ipv6_forwarding() -> Result<bool, io::Error> {
+        sysctl::value(IPV6_KEY)
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
+    }
+
+    #[inline]
+    pub fn enable_ipv6_forwarding() -> Result<bool, io::Error> {
+        sysctl::set_value(IPV6_KEY, sysctl::CtlValue::Int(1))
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
+    }
+
+    #[inline]
+    pub fn disable_ipv6_forwarding() -> Result<bool, io::Error> {
+        sysctl::set_value(IPV6_KEY, sysctl::CtlValue::Int(0))
+            .map(value_to_bool)
+            .map_err(|_| io::Error::last_os_error())?
     }
 }
 
-#[inline]
-pub fn ipv4_forwarding() -> Result<bool, io::Error> {
-    sysctl::value(IPV4_KEY)
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
+
+#[cfg(any(target_os = "android", target_os = "linux"))]
+mod sys {
+    use libc::{ c_int, c_void, };
+    
+    use std::io;
+    use std::ptr;
+
+    pub const CTL_NET: c_int = 3;        // Networking
+    
+    pub const NET_IPV4: c_int = 5;
+    pub const NET_IPV6: c_int = 12;
+
+    pub const NET_IPV4_FORWARD: c_int = 8;
+    pub const NET_IPV6_FORWARDING: c_int = 1;
+
+
+    #[inline]
+    pub fn ipv4_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV4, NET_IPV4_FORWARD, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 0;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let mut len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ipforward_ptr, &mut len, ptr::null_mut(), 0) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(ipforward == 1)
+    }
+
+
+    #[inline]
+    pub fn enable_ipv4_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV4, NET_IPV4_FORWARD, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 1;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ptr::null_mut(), &mut 0, ipforward_ptr, len) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        ipv4_forwarding()
+    }
+
+    #[inline]
+    pub fn disable_ipv4_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV4, NET_IPV4_FORWARD, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 0;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ptr::null_mut(), &mut 0, ipforward_ptr, len) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        ipv4_forwarding()
+    }
+
+    #[inline]
+    pub fn ipv6_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV6, NET_IPV6_FORWARDING, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 0;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let mut len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ipforward_ptr, &mut len, ptr::null_mut(), 0) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        Ok(ipforward == 1)
+    }
+
+    #[inline]
+    pub fn enable_ipv6_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV6, NET_IPV6_FORWARDING, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 1;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ptr::null_mut(), &mut 0, ipforward_ptr, len) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        ipv6_forwarding()
+    }
+
+    #[inline]
+    pub fn disable_ipv6_forwarding() -> Result<bool, io::Error> {
+        let mut mib = [ CTL_NET, NET_IPV6, NET_IPV6_FORWARDING, 0 ];
+        let mib_ptr: *mut c_int = mib.as_mut_ptr();
+        let mut ipforward: c_int = 0;
+        let ipforward_ptr = &mut ipforward as *mut i32 as *mut c_void;
+        let len = std::mem::size_of::<c_int>();
+        
+        if unsafe { libc::sysctl(mib_ptr, 4, ptr::null_mut(), &mut 0, ipforward_ptr, len) } < 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        ipv6_forwarding()
+    }
 }
 
-#[inline]
-pub fn enable_ipv4_forwarding() -> Result<bool, io::Error> {
-    sysctl::set_value(IPV4_KEY, sysctl::CtlValue::Int(1))
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
-}
-
-#[inline]
-pub fn disable_ipv4_forwarding() -> Result<bool, io::Error> {
-    sysctl::set_value(IPV4_KEY, sysctl::CtlValue::Int(0))
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
-}
-
-#[inline]
-pub fn ipv6_forwarding() -> Result<bool, io::Error> {
-    sysctl::value(IPV6_KEY)
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
-}
-
-#[inline]
-pub fn enable_ipv6_forwarding() -> Result<bool, io::Error> {
-    sysctl::set_value(IPV6_KEY, sysctl::CtlValue::Int(1))
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
-}
-
-#[inline]
-pub fn disable_ipv6_forwarding() -> Result<bool, io::Error> {
-    sysctl::set_value(IPV6_KEY, sysctl::CtlValue::Int(0))
-        .map(value_to_bool)
-        .map_err(|_| io::Error::last_os_error())?
-}
+pub use self::sys::*;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+#[cfg(target_os = "linux")]
+extern crate libc;
 extern crate ctrlc;
 extern crate mio;
 extern crate tun;
+#[cfg(target_os = "macos")]
 extern crate sysctl;
 extern crate smoltcp;
 extern crate crypto;
@@ -16,5 +19,9 @@ pub mod ip_forwarding;
 
 
 fn main() {
-    
+    println!("{:?}", ip_forwarding::ipv4_forwarding());
+    println!("{:?}", ip_forwarding::enable_ipv4_forwarding());
+    println!("{:?}", ip_forwarding::ipv4_forwarding());
+    println!("{:?}", ip_forwarding::disable_ipv4_forwarding());
+    println!("{:?}", ip_forwarding::ipv4_forwarding());
 }
