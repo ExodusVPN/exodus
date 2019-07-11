0677bed9498a0d1e1baf9038e72c3da731d687aa
luozijun <luozijun.assistant@gmail.com>
2019-07-11 19:27:42 +0800

    Update crates/sysctl

--- crates/sysctl/src/sys/mod.rs
+++ crates/sysctl/src/sys/mod.rs
@@ -1,4 +1,4 @@
-// #[cfg(any(target_os = "android", target_os = "linux"))]
+#[cfg(any(target_os = "android", target_os = "linux"))]
 pub mod linux;
 #[cfg(any(target_os = "android", target_os = "linux"))]
 pub use self::linux::*;
