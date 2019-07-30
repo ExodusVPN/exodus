f9a1ffab7e6f8f49332c712b033db3f93e70bcc7
luozijun <luozijun.assistant@gmail.com>
2019-07-30 14:40:43 +0800

    Update crates/netlink

--- crates/netlink/src/lib.rs
+++ crates/netlink/src/lib.rs
@@ -5,12 +5,10 @@ extern crate libc;
 extern crate bitflags;
 extern crate byteorder;
 
-#[allow(non_camel_case_types, non_upper_case_globals)]
-pub mod sys;
+
 pub mod packet;
 pub mod route;
 pub mod socket;
-
 
 #[inline]
 pub const fn alloc_response() -> [u8; packet::MAX_NL_LENGTH] {
