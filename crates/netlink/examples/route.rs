59d8be35bd55fb0d25692cc5b8bda8572e733008
luozijun <luozijun.assistant@gmail.com>
2019-08-17 20:33:58 +0800

    Update crates/netlink

--- crates/netlink/examples/route.rs
+++ crates/netlink/examples/route.rs
@@ -7,7 +7,41 @@ fn main() -> Result<(), io::Error> {
     let mut socket = netlink::route::RouteController::new()?;
     for x in socket.routes(&mut buffer)? {
         let item = x?;
-        println!("{:?}", item);
+        // println!("{:?}", item);
+        if item.kind != netlink::packet::RouteType::RTN_UNSPEC && item.kind != netlink::packet::RouteType::RTN_UNICAST {
+            continue;
+        }
+
+        if item.out_ifindex.is_none() {
+            continue;
+        }
+
+        let dst = if let Some(cidr) = item.dst_cidr {
+            format!("{}", cidr)
+        } else {
+            "default".to_string()
+        };
+
+        let src = if let Some(addr) = item.pref_src {
+            format!("{}", addr)
+        } else {
+            "None".to_string()
+        };
+
+        let out_ifindex = if let Some(index) = item.out_ifindex {
+            format!("{}", index)
+        } else {
+            "None".to_string()
+        };
+
+        println!("table={:16} protocol={:16} scope={:18} kind={:12} DST={:20} Gateway={:20} Link=#{}",
+            format!("{:?}", item.table),
+            format!("{:?}", item.protocol),
+            format!("{:?}", item.scope),
+            format!("{:?}", item.kind),
+            dst,
+            src,
+            out_ifindex);
     }
     
     Ok(())
